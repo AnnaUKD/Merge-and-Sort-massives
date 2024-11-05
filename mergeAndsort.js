c53d324af25eb980472dd40906124b7c3bec2c52
PogoreltsevaAnnaUKDIPZ <anna.i.pohoreltseva@ukd.edu.ua>
2024-11-06 00:49:51 +0200

    Update mergeAndsort.js

--- mergeAndsort.js
+++ mergeAndsort.js
@@ -1,10 +1,29 @@
-function merge (arr1, arr2){
+function merge(arr1, arr2, order = 'asc') {
     const result = [];
     let i = 0;
     let j = 0;
 
+    const compare = (a, b) => {
+        if (order === 'asc') {
+            return a < b;
+        } 
+        else {
+            return a > b;
+        }
+    };
+
+    
+    if (order === 'desc') {
+        arr1 = arr1.sort((a, b) => b - a);
+        arr2 = arr2.sort((a, b) => b - a);
+    } 
+    else {
+        arr1 = arr1.sort((a, b) => a - b);
+        arr2 = arr2.sort((a, b) => a - b);
+    }
+
     while (i < arr1.length && j < arr2.length) {
-        if (arr1[i] < arr2[j]) {
+        if (compare(arr1[i], arr2[j])) {
             result.push(arr1[i]);
             i++;
         } else {
@@ -24,7 +43,9 @@ function merge (arr1, arr2){
     }
 
     return result;
-};
+}
 
-console.log("Результат сортування:", merge([1, 4, 7], [-1, 0, 2]));
+
+
+console.log("Результат сортування за спаданням:", merge([1, 4, 7], [-1, 0, 2], 'asc'));
 
